8e0b100ad4d77e947b2fe86f0315f11912caeb2b
chen <ines.cyn@gmail.com>
2014-11-12 22:34:46 +0000

    bla

---
+++ test.php
@@ -0,0 +1,28 @@
+<img id="uploadPreview" style="width: 100px; height: 100px;" />
+<head>
+<script src="js/jquery.js"></script>
+</head>
+<body>
+<input id="uploadImage" type="file" name="myPhoto" onchange="PreviewImage();" />
+<div id="div1"></div>
+<script type="text/javascript">
+
+    function PreviewImage() {
+        var oFReader = new FileReader();
+        oFReader.readAsDataURL(document.getElementById("uploadImage").files[0]);
+
+        oFReader.onload = function (data) {
+           // document.getElementById("uploadPreview").src = oFREvent.target.result;
+           
+           // jQuery("#div1").load(oFREvent.target.result);
+
+           console.log(data.target.result);
+
+
+        };
+    };
+
+</script>
+
+
+</body>
